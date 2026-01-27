e19878cf198a7630da103146c35dbb6dc9ed2012
HarleenKaur812 <gs8606197@gmail.com>
2026-01-27 14:28:25 +0530

    Experiment 2

--- ecotrack/src/App.jsx
+++ ecotrack/src/App.jsx
@@ -1,16 +1,34 @@
+
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 import Logs from "./pages/Logs.jsx";
+import Login from "./pages/Login.jsx";
+import ProtectedRoutes from "./routes/ProtectedRoutes.jsx";
+import DashboardLayout from "./pages/DashboardLayout.jsx";
+import DashboardAnalytics from "./pages/DashboardAnalytics.jsx";
+import DashboardSummary from "./pages/DashboardSummary.jsx";
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Header  title="EcoTrack - Experiment 1" />
-      <main style={{ padding: "1rem" }}>
-        <Dashboard />
-        <Logs />
-      </main>
-    </>
+    <BrowserRouter>
+      <Header title="Ecotrack"/>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Login"/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={
+          <ProtectedRoutes>
+            <DashboardLayout/>
+          </ProtectedRoutes>
+        }>
+          <Route index element={<DashboardSummary/>}/>
+          <Route path="summary" element={<DashboardSummary/>}/>
+          <Route path="analytics" element={<DashboardAnalytics/>}/>
+          <Route path="logs" element={<ProtectedRoutes> <Logs/></ProtectedRoutes>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
+
 export default App;
--- ecotrack/src/components/Header.jsx
+++ ecotrack/src/components/Header.jsx
@@ -1,9 +1,63 @@
-const Header = ({title})=>{
-    return<>
-    <header style={{padding: "0.5rem",backgroundColor: "#3a803cff", color: "white", textAlign: "center"}}>
-    <h1>{title}</h1>
-    </header>
-    </> 
-}
+import { Link } from "react-router-dom";
+
+const Header = ({ title }) => {
+  return (
+    <>
+      <header
+        style={{
+          padding: "1rem",
+          backgroundColor: "#2ecc71",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        <h2>{title}</h2>
+        <nav>
+          <Link
+            to="/"
+            style={{
+              marginRight: "1rem",
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Dashboard
+          </Link>
+          <Link
+            to="/logs"
+            style={{
+              marginRight: "1rem",
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Logs
+          </Link>
+          <Link
+            to="/"
+            style={{
+              marginRight: "1rem",
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Login
+          </Link>
+          <Link
+            to="/"
+            style={{
+              marginRight: "1rem",
+              color: "red",
+              textDecoration: "none",
+            }}
+          >
+            Logout
+          </Link>
+          
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default Header;
